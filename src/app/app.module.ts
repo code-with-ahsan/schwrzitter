377a862e137ed72e22211ae799f6e8b213fa4353
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2023-08-30 18:59:00 +0200

    chore(firebase): update config

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+
+/**
+ * Our app is based on standalone components but we'll keep
+ * this file to install @angular/fire
+ */
+@NgModule({
+  imports: [BrowserModule, provideFirebaseApp(() => initializeApp(environment.firebase)), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())],
+})
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,22 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import {
   provideRouter,
   withEnabledBlockingInitialNavigation,
-} from '@angular/router';
-import { appRoutes } from './app/app.routes';
-import { AppComponent } from './app/app.component';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { provideFirestore, getFirestore } from '@angular/fire/firestore';
-// TODO: uncomment this
-// import { provideAuth, getAuth } from '@angular/fire/auth';
-import { importProvidersFrom } from '@angular/core';
+} from "@angular/router";
+import { appRoutes } from "./app/app.routes";
+import { AppComponent } from "./app/app.component";
 import { environment } from './environments/environment';
+import { initializeApp, provideFirebaseApp } from "@angular/fire/app";
+import { getFirestore, provideFirestore } from "@angular/fire/firestore";
+import { getAuth, provideAuth } from "@angular/fire/auth";
+import { importProvidersFrom } from "@angular/core";
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
-    importProvidersFrom(provideFirebaseApp(() => initializeApp(environment.firebase))),
-    importProvidersFrom(provideFirestore(() => getFirestore())),
-    // TODO: uncomment this
-    // importProvidersFrom(provideAuth(() => getAuth())),
+    importProvidersFrom([
+      provideFirebaseApp(() => initializeApp(environment.firebase)),
+      provideAuth(() => getAuth()),
+      provideFirestore(() => getFirestore())
+    ])
   ],
 }).catch((err) => console.error(err));
