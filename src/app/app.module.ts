99100ce50a9d1360c6805edd8854b07f7190c2b5
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2023-08-30 18:35:35 +0200

    chore: add app.module.ts

---
+++ src/app/app.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+/**
+ * Our app is based on standalone components but we'll keep
+ * this file to install @angular/fire
+ */
+@NgModule({
+  imports: [BrowserModule],
+})
+export class AppModule { }
